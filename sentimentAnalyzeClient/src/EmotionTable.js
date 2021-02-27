44a64b02831cd19ba2c15dfe65889ddf963a0d55
gguemes <74108503+gguemes@users.noreply.github.com>
2021-02-27 13:03:12 +0100

    Update

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -1,31 +1,36 @@
-import React from "react";
-import "./bootstrap.min.css";
+import React from 'react';
+import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
-  render() {
-    let emotions = Object.keys(this.props.emotions);
-    return (
-      <div>
-        {/*You can remove this line and the line below. */}
-        {/*JSON.stringify(this.props.emotions)*/}
-        {console.log(Object.keys(this.props.emotions))}
-        <table className="table table-bordered">
-          <tbody>
+    constructor(props) {
+        super(props);        
+        let emotions = [];
+        for (let key in props.emotions) {            
+            emotions.push([key, props.emotions[key]]);
+        }
+        console.log(emotions);
+        this.state = {emotionList:emotions}
+        console.log(this.state.emotionList);
+    }
+
+    render() {      
+      return (            
+        <div>
+          {/*You can remove this line and the line below. */}
+          {/*JSON.stringify(this.props.emotions)*/}
+          <table className="table table-bordered">
+            <tbody>
             {
-              //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
-              emotions.map((emotion) => {
-                return (
-                  <tr key={emotion}>
-                    <td>{emotion}</td>
-                    <td>{this.props.emotions[emotion]}</td>
-                  </tr>
-                );
-              })
-            }
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+                this.state.emotionList.map(
+                    function(emotion){
+                        return <tr><td>{emotion[0]}</td><td>{emotion[1]}</td></tr>;
+                    })
+            }            
+            </tbody>
+          </table>
+          </div>
+          );
+        }
+    
 }
 export default EmotionTable;
